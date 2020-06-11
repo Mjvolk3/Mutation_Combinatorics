02d244e11e6106a3f22793f139edba9fd58fc240
Mjvolk3 <48566106+Mjvolk3@users.noreply.github.com>
2020-06-10 22:40:38 -0500

    randomized plating integrated with other bioscreen C processing

---
+++ BC_rand_plate.R
@@ -0,0 +1,52 @@
+#Date: 2020.03.07
+#Author: Michael Volk 
+
+# sample_list_file <- ('BioscreenC/experiments/ex12/Mv_ex12_sample_list.csv')
+# sample_list_file is a sample list
+BC_rand_plate <- function(sample_list_file = NULL, bio_reps = 3){
+# arguments
+# bio_reps <- 3
+
+        exp_list <- read.csv(sample_list_file, colClasses = 'character')
+        exp_list_br <- data.frame(sapply(exp_list[[1]], str_c, "_br"))
+        exp_list_reps <- data.frame(sample = paste0(exp_list_br[[1]], rep(seq(1, bio_reps), each = nrow(exp_list))))
+        bio_reps_file <- paste0(str_split(sample_list_file,".csv")[[1]][1],"_bioreps.csv")
+        write.csv(exp_list_reps,bio_reps_file)
+        
+        
+        max_tech_rep <- 200 %% nrow(exp_list_reps)
+        num_WT_assign <- 200 - max_tech_rep
+        
+        exp_list_comb <- exp_list_reps[,1]
+        
+        for (i in 1:(max_tech_rep-1)) {
+                exp_list_comb <- c(exp_list_comb,exp_list_reps[,1])
+        }
+        
+        while (length(exp_list_comb) < 200) {
+                WT_char_sub <- substr(as.character(exp_list_reps[1,1]), 1,5)
+                for (val in exp_list_reps[,1]){
+                        
+                        current_sample <- substr(val,1,5)
+                        
+                        if (current_sample == (WT_char_sub) && length(exp_list_comb) != 200){
+                                exp_list_comb <- c(exp_list_comb,as.character(val))
+                        }
+                }
+        }
+        
+        m <- matrix(sample(exp_list_comb), nrow = 10, ncol = 20)
+        
+        local_dir <- paste0(str_c(str_split(sample_list_file, "/")[[1]][1:3], collapse = "/"),"/position_list")
+        dir.create(local_dir)
+        
+        for (i in exp_list_reps[,1]){
+                n <- which(m == i, arr.ind = TRUE)
+                n <- data.frame(n, row.name = i)
+                #commented until functionalized
+                id <- str_c(local_dir, sprintf("/%s.csv", i),collapse = "/")
+                print(id)
+                write.csv(n,id)
+        }
+}
+
